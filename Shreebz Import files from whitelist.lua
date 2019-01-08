748ae8377c2a423a25e80741ae70141fcd6a01c9
Shreebz <dschreiberjr@gmail.com>
2019-01-08 17:50:47 -0500

    Update Shreebz Import files from whitelist.lua

--- Shreebz Import files from whitelist.lua
+++ Shreebz Import files from whitelist.lua
@@ -19,32 +19,50 @@ OSX file format: /Users/dschreiberjr/Google Drive/1-DTS Audio Main/Reaper/Projec
 
 reaper.ClearConsole()
 
+copyPref = reaper.SNM_GetIntConfigVar("copyimpmedia", -666)
+--reaper.ShowConsoleMsg("Copy Media on Import is: "..pref.."\n")
+
+if copyPref ~= 1 then
+  reaper.ShowConsoleMsg("\"Copy imported media to project media directory\" preference is disabled, it is dangerous to run this script while disabled, so this script will not function until that option is enabled. Please turn it on. ".."\n".."\n".."Goto: Preferences -> Media -> \"Copy imported media to project media directory\". Enable it, it should be the top option. \n\nPlease be sure that your project media directory is defined too. If not, you will likely make a mess of your project folder. \n\n ")
+  goto WrongPref
+end
+
 userDistance = 5                        -- Distance, in seconds, between end of item and start of next
 x = 1                                   -- Variable for iterating table key
-file_names_table = {}                   -- Creating table of file names
-final_path_table = {}                   -- Global table for full path concatenated with file name
+whitelist_file_names = {}               -- Creating table of file names
+from_path_full = {}                     -- Global table for "from" full path concatenated with file name
 Track = reaper.GetLastTouchedTrack()    -- Get last touched track to copy files to
-log_name = " Import log.txt"
 filetype = ".wav"
-proj_path = reaper.GetProjectPath("").."/"
-reaper.ShowConsoleMsg("Project path: ".."\n"..proj_path.."\n".."\n")
+proj_path = reaper.GetProjectPath("").."/"  -- Get project path
+proj_name = reaper.GetProjectName(0, "")    -- Get project name
+log_name = " Import log - "..proj_name:gsub(".rpp","")..".txt" -- Log name, minus the .rpp extension
 
 -----------------
 --- Functions ---
 -----------------
 
+--[[
+  a: Extract path -- /Projects/Import File From Whitelist/Copy From Here/
+  b: Extract filename -- DTS_002_001_04 Stone Slide Drag Grind.wav
+  c: Extract filetype -- wav
+]]
 function StringExtraction(string)
   a, b, c = string:match("(.-)([^\\/]-%.?([^%.\\/]*))$")
 end
+--[[
+test_string = "/Users/dschreiberjr/Google Drive/1-DTS Audio Main/Reaper/Projects/Import File From Whitelist/Copy From Here/DTS_002_001_04 Stone Slide Drag Grind.wav"
+StringExtraction(test_string)
+reaper.ShowConsoleMsg("A: "..a.."\n".."B: "..b.."\n".."C: "..c.."\n".."\n")
+]]
 
 -- Save cursor position
 function SaveCursorPos()
-	init_cursor_pos = reaper.GetCursorPosition()
+  init_cursor_pos = reaper.GetCursorPosition()
 end
 
 -- Restore Cursor position
 function RestoreCursorPos()
-	reaper.SetEditCurPos(init_cursor_pos, false, false)
+  reaper.SetEditCurPos(init_cursor_pos, false, false)
 end
 function MoveCursorPostion(userDistance)
   local curPos = reaper.GetCursorPosition() -- Get cursor position
@@ -71,44 +89,82 @@ function trackSelection()
   end
 end
 
+function writeLog(s)
+log = io.open(proj_path..log_name,"w+")
+log:write(s.."\n")
+io.close(log)
+end
+
+-- Checks if file in whitelist (concatenated with from_path_full) is a valid, readable file.
+function validate_files()
+  valid_files_table = {}                     -- table of valid files (full path) for import
+  valid_log_files = {}                       -- table of valid files for log
+  invalid_files_table = {}                   -- table of missing files from import
+  whitelist = io.open(filetxt,"r")              -- opening the text file as read only
+  aa = 1                                        -- Main loop iterator
+  for s in whitelist:lines() do                 -- Start the loop: for each line "s" in the opened text file do something until end
+    s = removeCharsFromWhitelist(s)             -- remove return characters from file name
+    table.insert(from_path_full,formattedPath..s)   -- Concatenating path with file name from whitelist to table: from_path_full
+    --reaper.ShowConsoleMsg("Inserted to from_full_path table: "..from_path_full[aa].."\n")
+    if reaper.file_exists(from_path_full[aa]) then          -- If the file is valid, then insert it to its own "valid" table
+      table.insert(valid_files_table, formattedPath..s)     -- Valid files path
+      table.insert(valid_log_files, s)                      -- Valid files for log
+    else                                                    -- If the file is invalid, then insert to its own "invalid" table
+      table.insert(invalid_files_table, s)                  -- Missing files for log
+    end
+    aa = aa + 1                                             -- Increment iterator
+  end
+  io.close(whitelist)
+end
+
+local function printTable(t,title)
+  reaper.ShowConsoleMsg(title.."\n")
+  for k, v in pairs(t) do
+    reaper.ShowConsoleMsg(k .. "\t" .. v .. "\n")
+  end
+  reaper.ShowConsoleMsg("\n")
+end
+
 SaveCursorPos() -- Save initial cursor position
 
 reaper.PreventUIRefresh(1)
 reaper.Undo_BeginBlock()
 
--- Opens file browser window. Text file to read.
-userWhitelistBool, filetxt = reaper.GetUserFileNameForRead("", "Whitelist", "txt") 
-reaper.ShowConsoleMsg("Text file used: " .. tostring(filetxt) .. "\n" .. "\n")
-
+userWhitelistBool, filetxt = reaper.GetUserFileNameForRead("", "Whitelist", "txt") -- Opens browser window for whitelist text file.
+--reaper.ShowConsoleMsg("Text file used: " .. tostring(filetxt) .. "\n" .. "\n")
 --reaper.ShowConsoleMsg(tostring("Init cursor pos: "..init_cursor_pos).."\n".."\n")
-
-if userWhitelistBool then
+if userWhitelistBool then                               -- If the whitelist prompt is Okay'd then continue....
   userPathBool, importedPath = reaper.GetUserFileNameForRead("", "File Path", "wav") -- Opens file browser window. Whitelist text file to read.
-  if userPathBool then
+  if userPathBool then                                  -- If the folder prompt is Okay'd then continue...
     formattedPath = getPath(importedPath)
     trackSelection()
-    whitelist = io.open(filetxt,"r")        -- opening the text file as read only
-    log = io.open(proj_path..log_name,"w+")
-      for s in whitelist:lines() do         -- Start the loop: for each line "s" in the opened text file do something until end
-      s = removeCharsFromWhitelist(s)       -- remove return characters from file name
-      table.insert(file_names_table,s)      -- Make table for file name
-      table.insert(final_path_table,formattedPath..s..filetype) -- Concatenating path with file name from whitelist to new table
-      reaper.InsertMedia(final_path_table[x],0)       -- Insert filename
-      reaper.ShowConsoleMsg("File Name: "..file_names_table[x].."\n")
-      reaper.ShowConsoleMsg("Full Path: "..final_path_table[x].."\n".."\n")
-      MoveCursorPostion(userDistance)       -- Moves edit cursor position forward by 5
-      --log:write(s.."\n")                    -- Write log
-      x = x + 1                             -- Increment array index
+    validate_files()
+    --whitelist = io.open(filetxt,"r")                  -- opening the text file as read only
+    for k, v in pairs(valid_files_table) do             -- Loop through the valid files table
+      reaper.InsertMedia(v, 0)                          -- Insert media from validate list
+      MoveCursorPostion(userDistance)                   -- Move cursor position forward
+      --reaper.ShowConsoleMsg("V is: "..v.."\n")
     end
     reaper.TrackList_AdjustWindows(false)
     reaper.PreventUIRefresh(-1)
     reaper.UpdateArrange()
     reaper.UpdateTimeline()
-    io.close(whitelist)
-    io.close(log)
-    reaper.Main_OnCommand(40290, 1)         -- Move edit cursor to start of time selection.
+    reaper.Main_OnCommand(40290, 1)                     -- Move edit cursor to start of time selection.
     RestoreCursorPos()
     reaper.Undo_EndBlock("Remove list",1)
+
+    
+    log = io.open(proj_path..log_name,"w+")             -- Create import log text file in project media directory.
+    log:write("Project name: "..proj_name.."\n".."Project path: "..proj_path.."\n")
+    log:write("\n".."\n".."\n".."These files were not imported: ")
+    for k, v in pairs(invalid_files_table) do
+      log:write("\n"..v)                                -- Write log for invalid files
+    end
+    log:write("\n".."\n".."\n".."\n".."Successfully added these files: ")
+    for k, v in pairs(valid_log_files) do
+      log:write("\n"..v)                                -- Write log for valid files
+    end
+    io.close(log)
   else
     -- do nothing
   end
@@ -116,3 +172,6 @@ if userWhitelistBool then
 else
   -- do nothing
 end
+
+::WrongPref::
+
